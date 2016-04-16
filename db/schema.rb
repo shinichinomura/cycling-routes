b2bf8218ef5662f00a55b58ab72ee59be5166072
Shinichi NOMURA <nomura@crowdworks.co.jp>
2016-04-16 18:27:37 +0900

    目的地モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416090957) do
+ActiveRecord::Schema.define(version: 20160416092229) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -45,5 +45,13 @@ ActiveRecord::Schema.define(version: 20160416090957) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "destinations", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "address",     limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
 end
