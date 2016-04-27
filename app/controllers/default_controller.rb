6fef6b3fcc182b073604b8aad33bcc08199be3f4
Shinichi NOMURA <shinichi.nomura@gmail.com>
2016-04-27 20:38:31 +0900

    resolve #24 Google用のサイトマップを生成する

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -2,4 +2,8 @@ class DefaultController < ApplicationController
   def index
     redirect_to destinations_url
   end
+
+  def sitemap
+    render :sitemap, content_type: 'application/xml'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
+  get 'sitemap' => 'default#sitemap'
+
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
