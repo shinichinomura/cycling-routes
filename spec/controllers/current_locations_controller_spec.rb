146e3567954a4dd7a83a9697d0cc1f3321b32304
Shinichi NOMURA <shinichi.nomura@gmail.com>
2016-04-29 13:33:36 +0900

    current_locations#createのテストを記述

--- spec/controllers/current_locations_controller_spec.rb
+++ spec/controllers/current_locations_controller_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe CurrentLocationsController, type: :controller do
     end
   end
 
+  describe "POST #create" do
+    it do
+      post :create, suggested_address: "東京都渋谷区"
+
+      expect(session[:current_location]).to eq('東京都渋谷区')
+    end
+  end
 end
